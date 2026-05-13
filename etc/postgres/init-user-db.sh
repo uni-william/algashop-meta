ba3099820d05fee7d4e5deb50abd7d9517f1be84
William Gomes <william.desenv@gmail.com>
2026-05-13 12:31:13 -0400

    Atualizando projeto apos reconstrucao no git no novo repositorio

---
+++ etc/postgres/init-user-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE DATABASE ordering;
+	CREATE DATABASE billing;
+	CREATE DATABASE ordering_test;
+  CREATE DATABASE billing_test;
+  CREATE DATABASE fastpay;
+EOSQL
---
+++ etc/stub-runner/run-product-catalog-stub.sh
@@ -0,0 +1,3 @@
+java -jar stub-runner.jar \
+  --stubrunner.ids=com.algaworks.algashop:product-catalog:0.0.1-SNAPSHOT:8083 \
+  --stubrunner.stubs-mode=LOCAL
