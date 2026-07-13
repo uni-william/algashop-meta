a43ecdae1d5025cb836a666e3e2b618975528cd6
William Gomes <william.desenv@gmail.com>
2026-07-13 11:35:42 -0400

    Update

--- etc/postgres/init-user-db.sh
+++ etc/postgres/init-user-db.sh
@@ -2,9 +2,11 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE DATABASE fastpay;
 	CREATE DATABASE ordering;
 	CREATE DATABASE billing;
+	CREATE DATABASE authorization_server;
 	CREATE DATABASE ordering_test;
   CREATE DATABASE billing_test;
-  CREATE DATABASE fastpay;
+	CREATE DATABASE authorization_server_test;
 EOSQL
